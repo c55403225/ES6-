999fbcd6c56ea7750bb0083ac3c8c292e5698745
c55403225 <55403225@qq.com>
2020-09-02 16:11:45 +0800

    ES6模块化

---
+++ index.js
@@ -0,0 +1,4 @@
+import {str,obj,fn} from './module.js';
+console.log('str',str);
+console.log('nama',obj.name);
+console.log('fn',fn)
---
+++ module.js
@@ -0,0 +1,8 @@
+let str = 'ChenLiPeng';
+let obj = {
+    name : 'clp'
+};
+let fn = () => {
+    console.log('moudle test');
+};
+export {str,obj,fn}
